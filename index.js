86c703c35c7c000e049b868af424034a3731dee8
Prince Thind <princethind799@gmail.com>
2022-09-16 13:07:50 +0530

    implement populate calender

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const generateCalender=require('./lib/generateCalender');
-const calender=generateCalender()
+const populateCalender = require('./lib/populateCalender');
+const calender=generateCalender();
+
+populateCalender(calender);
+
+console.log(calender.slice(0,1))
 
