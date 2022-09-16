e8301429438a3c45e8806637a4c01a522e854bec
Prince Thind <princethind799@gmail.com>
2022-09-16 14:29:36 +0530

    implement csv generation code

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const generateCalender=require('./lib/generateCalender');
+const generateCSV = require('./lib/generateCSV');
 const generateReport = require('./lib/generateReport');
 const populateCalender = require('./lib/populateCalender');
+
 const calender=generateCalender();
 
 populateCalender(calender);
 
 const report=generateReport(calender);
+
+generateCSV(report);
 
 
