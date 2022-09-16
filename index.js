4d448296fbdc5ccb18b14b802a25b29f7ed3deeb
Prince Thind <princethind799@gmail.com>
2022-09-16 14:12:37 +0530

    implement report generation

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const generateCalender=require('./lib/generateCalender');
+const generateReport = require('./lib/generateReport');
 const populateCalender = require('./lib/populateCalender');
 const calender=generateCalender();
 
 populateCalender(calender);
 
-console.log(calender.slice(0,1))
+const report=generateReport(calender);
+
 
