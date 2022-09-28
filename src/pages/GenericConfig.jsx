b01e4e6c448d83b9d9d9a8ec65e684f40f902bbd
Prince Thind <princethind799@gmail.com>
2022-09-28 20:09:54 +0530

    add json validation

--- src/pages/GenericConfig.jsx
+++ src/pages/GenericConfig.jsx
@@ -4,23 +4,31 @@ export default function GenericConfig({ config, setConfig, type }) {
   const [textData, setTextData] = useState(
     JSON.stringify(config[type], null, 1)
   );
+  const [error, setError] = useState(null);
 
   return (
     <div className="module">
       <h2>Configuration File: {type}</h2>
       <form onSubmit={saveConfig}>
         <textarea onChange={updateTextData} value={textData}></textarea>
+        <p className="error"> {error}</p>
         <button>Save</button>
       </form>
     </div>
   );
 
   function updateTextData(e) {
-    setTextData(e.target.value);
+    const json = e.target.value;
+    const jsonError = checkJSONValidity(json);
+
+    setTextData(json);
+    setError(jsonError);
   }
 
   function saveConfig(e) {
     e.preventDefault();
+    if (error) return alert('please fix the errors first!');
+
     setConfig((config) => {
       const configCopy = JSON.parse(JSON.stringify(config ?? {}));
       configCopy[type] = JSON.parse(textData);
@@ -28,4 +36,13 @@ export default function GenericConfig({ config, setConfig, type }) {
     });
     alert("saved!");
   }
+}
+
+function checkJSONValidity(json) {
+  try {
+    JSON.parse(json);
+  } catch (e) {
+    return e.message;
+  }
+  return null;
 }
