cfb540142f506e5e608c96d29a3f089cb7518471
Prince Thind <princethind799@gmail.com>
2023-01-05 22:25:09 +0530

    add midterm week to holidays

--- src/lib/sampleConfig.js
+++ src/lib/sampleConfig.js
@@ -6,7 +6,18 @@ const config = {
     showOnlyDaysWithClasses: true,
     fileName: "prince",
   },
-  holidays: ["2023-01-26", "2023-03-08", "2023-04-22"],
+  holidays: [
+    "2023-01-26",
+    "2023-03-08",
+    "2023-04-22",
+    "2023-02-26",
+    "2023-02-27",
+    "2023-02-28",
+    "2023-03-01",
+    "2023-03-02",
+    "2023-03-03",
+    "2023-03-04",
+  ],
   makeups: {},
   teachersOnLeave: {},
   weekStructure: [
@@ -16,43 +27,42 @@ const config = {
       int416: 0,
       psy802: 0,
     },
-     {
+    {
       int301: 2,
       int411: 1,
       int416: 2,
       psy802: 1,
     },
-     {
+    {
       int301: 0,
       int411: 1,
       int416: 0,
       psy802: 1,
     },
-     {
+    {
       int301: 2,
       int411: 1,
       int416: 1,
       psy802: 1,
     },
-     {
+    {
       int301: 0,
       int411: 0,
       int416: 0,
       psy802: 0,
     },
-     {
+    {
       int301: 0,
       int411: 0,
       int416: 0,
       psy802: 0,
     },
-     {
+    {
       int301: 0,
       int411: 0,
       int416: 0,
       psy802: 0,
     },
-    
   ],
 };
 
